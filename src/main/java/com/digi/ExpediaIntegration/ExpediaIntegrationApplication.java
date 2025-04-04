7494912c67c195bd2de7c88fd9b371ae5877bcf8
Lenovo <Lenovo@DESKTOP-U643LIF>
2025-04-05 00:03:02 +0300

    First commit

---
+++ src/main/java/com/digi/ExpediaIntegration/ExpediaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.digi.ExpediaIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpediaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpediaIntegrationApplication.class, args);
+	}
+
+}
